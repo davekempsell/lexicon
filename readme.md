684caf7cb0e664b4baaae14b4891cdb15311f2bc
davekempsell <dave_kempsell@hotmail.com>
2022-09-28 16:48:36 +0100

    basic page layout created with title, grids, and keyboard buttons

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Lexicon
+Lexicon is a wordle clone, built to demonstrate a React front end app with a MongoDB Atlas back end, powered by Mongo Realm SAAS. The app has gone through several iterations in it's design and implementation. With the stages outlined below.
+
+## Iterations
+
+1. Basic Front end React App
+
+A single page app that takes input from clicking on buttons to change the displayed output on the page
--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Logo from './lexBrickLogo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="game">
+      <div class="title">
+        <img src={Logo} class="logo" />
+        <h1>LEXICON</h1>
+      </div>
+      <div id="grid-1" class="grid">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
+      <div id="grid-2" class="grid">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
+      <div id="grid-3" class="grid">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
+      <div id="grid-4" class="grid">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
+      <div id="grid-5" class="grid">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
+      <div id="grid-6" class="grid">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
+      <div class="keyboard-top">
+        <button type="submit">Q</button>
+        <button type="submit">W</button>
+        <button type="submit">E</button>
+        <button type="submit">R</button>
+        <button type="submit">T</button>
+        <button type="submit">Y</button>
+        <button type="submit">U</button>
+        <button type="submit">I</button>
+        <button type="submit">O</button>
+        <button type="submit">P</button>
+      </div>
+      <div class="keyboard-middle">
+        <button type="submit">A</button>
+        <button type="submit">S</button>
+        <button type="submit">D</button>
+        <button type="submit">F</button>
+        <button type="submit">G</button>
+        <button type="submit">H</button>
+        <button type="submit">J</button>
+        <button type="submit">K</button>
+        <button type="submit">L</button>
+      </div>
+      <div class="keyboard-bottom">
+      <button type="submit">
+          del
+        </button>
+        <button type="submit">Z</button>
+        <button type="submit">X</button>
+        <button type="submit">C</button>
+        <button type="submit">V</button>
+        <button type="submit">B</button>
+        <button type="submit">N</button>
+        <button type="submit">M</button>
+        <button type="submit">Ent</button>
+      </div>
+      <div class="footer">
+        <p>Built by Dave Kempsell 2022</p>
+      </div>
     </div>
   );
 }
