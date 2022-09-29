7ef5c8bdb44cd4f76828f28f9a84eb725c32f653
davekempsell <dave_kempsell@hotmail.com>
2022-09-29 10:43:31 +0100

    keyboard components refactored into separate file

---
+++ src/components/keyboard.js
@@ -0,0 +1,48 @@
+export function KeyboardTop() {
+  return (
+    <div className="keyboard-top" key="keyboard-top">
+      <button type="submit">Q</button>
+      <button type="submit">W</button>
+      <button type="submit">E</button>
+      <button type="submit">R</button>
+      <button type="submit">T</button>
+      <button type="submit">Y</button>
+      <button type="submit">U</button>
+      <button type="submit">I</button>
+      <button type="submit">O</button>
+      <button type="submit">P</button>
+    </div>
+  )
+}
+
+export function KeyboardMiddle() {
+  return (
+    <div className="keyboard-middle">
+      <button type="submit">A</button>
+      <button type="submit">S</button>
+      <button type="submit">D</button>
+      <button type="submit">F</button>
+      <button type="submit">G</button>
+      <button type="submit">H</button>
+      <button type="submit">J</button>
+      <button type="submit">K</button>
+      <button type="submit">L</button>
+    </div>
+  )
+}
+
+export function KeyboardBottom() {
+  return (
+    <div className="keyboard-bottom">
+      <button type="submit">del</button>
+      <button type="submit">Z</button>
+      <button type="submit">X</button>
+      <button type="submit">C</button>
+      <button type="submit">V</button>
+      <button type="submit">B</button>
+      <button type="submit">N</button>
+      <button type="submit">M</button>
+      <button type="submit">Ent</button>
+    </div>
+  )
+}
