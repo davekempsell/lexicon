883f78ae2020c649cdda5db9afab914fdd482473
davekempsell <dave_kempsell@hotmail.com>
2022-10-05 11:11:32 +0100

    frontend folder removed from route directory in server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000
-const publicPath = path.join(__dirname, '../frontend', 'build');
+const publicPath = path.join(__dirname, '..', 'build');
 
 app.use(express.static(publicPath));
 
