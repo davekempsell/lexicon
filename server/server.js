c778e1d393fdca0e89780577fe516be6f1843201
davekempsell <dave_kempsell@hotmail.com>
2022-10-05 10:42:55 +0100

    server points to build folder, not public

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 5000
+const publicPath = path.join(__dirname, '../frontend', 'build');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port
+  }`)
+})
+
